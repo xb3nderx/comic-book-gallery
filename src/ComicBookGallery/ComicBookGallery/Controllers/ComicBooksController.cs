d6b28c5adb091362247c3adad3f37e08d9d3adf8
xbender_x <facundoaraujo@gmail.com>
2022-10-18 15:15:25 -0300

    Updated Comic Detail View

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,6 +12,17 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue.... if spiderman survives <strong>Will Peter parker survive???</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script : Dant Slott",
+                "Pencil : Humberto Ramos",
+                "Inks   : Victor Olazaba",
+                "Colors : Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
             return View();
         }
     }
